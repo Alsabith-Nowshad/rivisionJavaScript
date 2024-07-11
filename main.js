bbe520f78ec36a66ac561bacba8ca48d8e9bb456
Alsabith-Nowshad <alsabithnowshad123@gmail.com>
2024-07-11 20:26:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,73 +1,148 @@
 
 
-Object.assign===================================================
+// Object.assign===================================================
 
 
-obj1 ={
-    name : "alsabith",
-    place : "ktm"
-}
+// obj1 ={
+//     name : "alsabith",
+//     place : "ktm"
+// }
 
 
-obj2 = {
-    name:"alsabith nowshad",
-    age : "18",
-    postions : "CEO"
-}
+// obj2 = {
+//     name:"alsabith nowshad",
+//     age : "18",
+//     postions : "CEO"
+// }
 
 
-hai = Object.assign(obj1,obj2)
+// hai = Object.assign(obj1,obj2)
 
 
-console.log(hai);
+// console.log(hai);
 
 
-object.create============================================================
+// object.create============================================================
 
-let newObject = Object.create(obj)
+// let newObject = Object.create(obj)
 
-obj.name = "alsabith"
+// obj.name = "alsabith"
 
-console.log(obj);
-
-
+// console.log(obj);
 
 
-object.entries==============================================================
-
-let object1 = {
-    name:"alsabith",
-    age:18
-}
-
-let obejct2 = {
-    place:"ktm",
-    position :"CEO"
-}
-
-let arrayObj = Object.entries(object1)
-
-console.log(arrayObj);
 
 
-object.fromEntries=====================================================
+// object.entries==============================================================
 
-let object1 = [
-    ['name' ,'alsabith'],
-    ['age', 18]
-]
+// let object1 = {
+//     name:"alsabith",
+//     age:18
+// }
 
-let arrayObj = Object.fromEntries(object1)
+// let obejct2 = {
+//     place:"ktm",
+//     position :"CEO"
+// }
 
-console.log(arrayObj);
+// let arrayObj = Object.entries(object1)
+
+// console.log(arrayObj);
 
 
-object.value=================================================
+// object.fromEntries=====================================================
 
-let object1 = {
-    name:"alsabith",
-    age:18
-}
+// let object1 = [
+//     ['name' ,'alsabith'],
+//     ['age', 18]
+// ]
 
-console.log(Object.values(object1));
+// let arrayObj = Object.fromEntries(object1)
+
+// console.log(arrayObj);
+
+
+// object.value=================================================
+
+// let object1 = {
+//     name:"alsabith",
+//     age:18
+// }
+
+// console.log(Object.values(object1));
+
+
+// self invoking function==========================================
+
+// (function add(hai){
+//     hai; 
+//     console.log(hai);
+// })(2)
+
+
+// closure===================================
+
+// function add(hai){
+//     h=hai; 
+    
+//     function result(){
+// console.log(h);
+//     }
+//     result()
+// }
+// add(123)
+
+
+
+// let arrSort = [
+//     23, 52, 74, 12, 3645, 12, 5, 2
+// ]
+
+// let sorted = arrSort.Sort()
+
+// console.log(sorted);
+
+
+// switch case====================================================
+
+// let x = "d";
+// let res;
+
+// switch(x){
+//     case 'a' : 
+//     res = "its a"
+//     break;
+
+//     case 'b' : 
+//     res = "its b"
+//     break;
+
+//     case 'c' : 
+//     res = "its c"
+//     break;
+
+//     case 'd' : 
+//     res = "its d"
+//     break;
+    
+//     case 'e' : 
+//     res = "its e"
+//     break;
+// }
+// console.log(res);
+    
+// while loop=========================================================
+
+// let i = 5;
+// while(i<10){
+//     console.log(i);
+//     i++
+// }
+
+// array destructuring===================================================
+
+// let fruits = ["apple","orange","grapes"]
+// let [first,second,third] = fruits
+
+// console.log(first);
 
