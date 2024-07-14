32b194e37126a6e73eca98c6395c52f76f021f27
Alsabith-Nowshad <alsabithnowshad123@gmail.com>
2024-07-14 21:53:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -146,3 +146,23 @@
 
 // console.log(first);
 
+// let name = "asd asdsa alsabith sdasd asd as "
+// result = name.search("alsabith")
+// console.log(result);
+
+
+// arr = [1,2,3,4,5]
+// let result  = arr.join("*")
+// console.log(result)
+
+k=1
+
+for(i=1;i<=3;i++){
+    for(j=1;j<=i;j++)
+    {
+        document.write(k)
+        k++
+        
+    }
+    document.write("<br>")
+}
---
+++ new.js
@@ -0,0 +1,24 @@
+// let arr = [1,2,3,4,7,7,7,8,8,8,5,6,2,23,3,3,4,5,5,5,6,7,7,7,8,8]
+
+// let sor = arr.sort((a,b)=>a-b)
+// arr2 = [...new Set(sor)]
+
+// console.log(arr2);
+
+
+// let num = [12,3,44,23,54,32]
+// let num2 = num.map(function(a){
+//    let sum =  a * 2
+//     return sum
+// }) 
+
+// console.log(num2);
+
+let num = [12,3,44,23,54,32]
+let filtered = num.filter(function(n){
+   New =  n  !=12
+   return New
+})
+
+console.log(filtered);
+
